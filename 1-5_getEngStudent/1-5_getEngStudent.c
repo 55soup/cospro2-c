f2784a7cce0c8be2ea2a2e4efc5ed6582f07eb6c
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-07-13 22:01:59 +0900

    [1차][한줄] 문제5) 영어 수강 대상자 수 구하기 구현완료

--- 1-5_getEngStudent/1-5_getEngStudent.c
+++ 1-5_getEngStudent/1-5_getEngStudent.c
@@ -2,6 +2,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 // [1차][한줄] 문제5) 영어 수강 대상자 수 구하기
+/*초급 영어 강의는 토익시험에서 650점 이상 800점 미만의 성적을 취득한 학생만을
+수강대상으로 하고 있습니다.*/
+int solution(int scores[], int scores_len) {
+    int answer = 0;
+    for (int i = 0; i < scores_len; i++) {
+        if (scores[i] >= 650 && scores[i] < 800) {
+            ++answer;
+        }
+    }
+    return answer;
+}
 
 int main() {
     int scores[10] = { 650, 722, 914, 558, 714, 803, 650, 679, 669, 800 };
