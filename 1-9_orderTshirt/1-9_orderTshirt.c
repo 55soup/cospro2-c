038030fb37a1c7b9c67ba620ad8a4a438ccb56f9
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-07-17 23:51:47 +0900

    [1차][구현] 문제9) 단체 티셔츠 주문하기 구현

--- 1-9_orderTshirt/1-9_orderTshirt.c
+++ 1-9_orderTshirt/1-9_orderTshirt.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+
 // [1차][구현] 문제9) 단체 티셔츠 주문하기
 
-int solution(char* shirt_size, int shirt_size_len) {
-
+int solution(char* shirt_size[], int shirt_size_len) {
+	int* answer = (int*)malloc(sizeof(int) * 101);
+	for (int i = 0; i < shirt_size_len; i++) {
+		if (strcmp(shirt_size[i], "XS") == 0) ++answer[0];
+		else if (strcmp(shirt_size[i], "S") == 0) ++answer[1];
+		else if (strcmp(shirt_size[i], "M") == 0) ++answer[2];
+		else if (strcmp(shirt_size[i], "L") == 0) ++answer[3];
+		else if (strcmp(shirt_size[i], "XL") == 0) ++answer[4];
+		else ++answer[5];
+	}
+	return answer;
 }
+
 int main() {
 	char* shirt_size[] = { "XS", "S", "L", "L", "XL", "S" };
 	int shirt_size_len = 6;
